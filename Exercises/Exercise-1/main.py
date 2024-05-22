9a7cb6b7d3a113f25baf56ee194a284126db85a8
ejaraminas <elvis.jaraminas@gmail.com>
2024-05-23 08:21:43 +1000

    final version

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
-import requests
+import os
+import zipfile
+import aiohttp
+import asyncio
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,10 +14,53 @@ download_uris = [
 ]
 
 
-def main():
-    # your code here
-    pass
+def add_directory(dir_name):
+    """Create new downloads directory"""
+    if os.path.isdir(dir_name):
+        print("Folder exists. Continuing.")
+    else:
+        os.mkdir(dir_name)
+        print("Folder created. Continuing.")
+    os.chdir(dir_name)
+
+
+async def download_file(uri):
+    """Use async download to retrieve file"""
+    file_name = uri.rsplit("/")[3]
+    print(f"Retrieving: {file_name}", end="")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(uri) as response:
+            if response.status == 200:
+                with open(file_name, 'wb') as f:
+                    while True:
+                        chunk = await response.content.read(1024)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+            else:
+                print(f" - Failed to download. Status code: {response.status}", end="")
+    print("--> Done.")
+
+
+async def main():
+
+    add_directory("downloads")
+
+    """Attempt downloading each zip"""
+    for uri in download_uris:
+        await download_file(uri)
+
+    """Extract CSV file from each zip file"""
+    zip_list = os.listdir()
+
+    for zip_file in zip_list:
+        zf = zipfile.ZipFile(zip_file, 'r')
+        zf_file_list = zf.namelist()
+        for file in zf_file_list:
+            if ".csv" in file:
+                zf.extract(file)
+        os.remove(zip_file)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
