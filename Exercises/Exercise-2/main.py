114ff272f016655cd42115058f9218fd1877db4c
ejaraminas <elvis.jaraminas@gmail.com>
2024-07-03 21:11:29 +1000

    final version

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -5,11 +5,8 @@ import pandas as pd
 
 def retrieve_page(url, timeout):
     """Retrieve page content and store as DataFrame"""
-    try:
-        r = requests.get(url, timeout=timeout).text
-        page_content_df = pd.read_html(io.StringIO(r))[0]    
-    except requests.exceptions.HTTPError as e:
-        print(e)
+    r = requests.get(url, timeout=timeout).text
+    page_content_df = pd.read_html(io.StringIO(r))[0]
     return page_content_df
 
 
@@ -21,14 +18,13 @@ def retrieve_file(url, file_name, timeout):
 
 
 def main():
-    
     url = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
-    file_timestamp = "2024-01-19 10:13" # Modified timestamp as original one doesn't exist...
+    file_timestamp = "2024-01-19 10:13"  # Modified timestamp as original one doesn't exist
 
     page_df = retrieve_page(url, 10)
     csv_file = page_df[page_df['Last modified'] == file_timestamp]['Name'].iloc[0]
     file_url = url + csv_file
-    
+
     retrieve_file(file_url, csv_file, 10)
     weather_data = pd.read_csv(csv_file)
 
